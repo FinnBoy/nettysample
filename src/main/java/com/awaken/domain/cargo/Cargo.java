f288396d6b98a4995a7b43e0934a714be6a0fc90
FinnBoy <finn0918@163.com>
2018-03-25 04:50:48 +0800

    build abstract entity.

--- src/main/java/com/awaken/domain/cargo/Cargo.java
+++ src/main/java/com/awaken/domain/cargo/Cargo.java
@@ -1,5 +1,6 @@
 package com.awaken.domain.cargo;
 
+import com.awaken.domain.holder.Holder;
 import com.awaken.domain.product.Sku;
 
 /**
@@ -11,7 +12,34 @@ import com.awaken.domain.product.Sku;
  */
 public abstract class Cargo {
 
+    /**
+     * 容器
+     */
+    private Holder holder;
+
+    /**
+     * 品项
+     */
     private Sku sku;
+
+    /**
+     * 状态
+     */
+    private Integer skuStatus;
+
+    /**
+     * 周转状态
+     * <p>库存、待上架、待拣货</p>
+     */
+    private Integer turnoverStatus;
+
+    public Holder getHolder() {
+        return holder;
+    }
+
+    public void setHolder(Holder holder) {
+        this.holder = holder;
+    }
 
     public Sku getSku() {
         return sku;
@@ -19,5 +47,21 @@ public abstract class Cargo {
 
     public void setSku(Sku sku) {
         this.sku = sku;
+    }
+
+    public Integer getSkuStatus() {
+        return skuStatus;
+    }
+
+    public void setSkuStatus(Integer skuStatus) {
+        this.skuStatus = skuStatus;
+    }
+
+    public Integer getTurnoverStatus() {
+        return turnoverStatus;
+    }
+
+    public void setTurnoverStatus(Integer turnoverStatus) {
+        this.turnoverStatus = turnoverStatus;
     }
 }

a76dcd2a9132559863f1096403bc70ab512d2018
FinnBoy <finn0918@163.com>
2018-03-27 23:59:02 +0800

    build abstract entity.

---
+++ src/main/java/com/awaken/domain/owner/Company.java
@@ -0,0 +1,31 @@
+package com.awaken.domain.owner;
+
+import com.awaken.domain.warehouse.Warehouse;
+
+import java.util.Set;
+
+/**
+ * 公司，企业
+ */
+public class Company {
+
+    private Set<Location> locations;
+
+    private Set<Warehouse> warehouses;
+
+    public Set<Location> getLocations() {
+        return locations;
+    }
+
+    public void setLocations(Set<Location> locations) {
+        this.locations = locations;
+    }
+
+    public Set<Warehouse> getWarehouses() {
+        return warehouses;
+    }
+
+    public void setWarehouses(Set<Warehouse> warehouses) {
+        this.warehouses = warehouses;
+    }
+}

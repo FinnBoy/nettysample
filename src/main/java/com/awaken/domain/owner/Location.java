f134ba983c7bf97f527e53add02660f829f0f58d
FinnBoy <finn0918@163.com>
2018-03-28 22:36:41 +0800

    build abstract entity.

--- src/main/java/com/awaken/domain/owner/Location.java
+++ src/main/java/com/awaken/domain/owner/Location.java
@@ -4,4 +4,14 @@ package com.awaken.domain.owner;
  * 地点
  */
 public class Location {
+
+    private Company company;
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
 }

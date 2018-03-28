81b1ed5c2f489d4c2d73f5f4af3b66ff89c0e06a
FinnBoy <finn0918@163.com>
2018-03-28 22:39:26 +0800

    build abstract entity.

--- src/main/java/com/awaken/domain/cargo/Batch.java
+++ src/main/java/com/awaken/domain/cargo/Batch.java
@@ -5,4 +5,14 @@ package com.awaken.domain.cargo;
  * <p>需要有拆批功能，及自动隐性拆批</p>
  */
 public class Batch extends Cargo {
+
+    private String number;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
 }
--- src/main/java/com/awaken/domain/cargo/Singleton.java
+++ src/main/java/com/awaken/domain/cargo/Singleton.java
@@ -4,4 +4,14 @@ package com.awaken.domain.cargo;
  * 单品
  */
 public class Singleton extends Cargo {
+
+    private String number;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
 }
